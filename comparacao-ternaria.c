9bb46ea58d6e973b7d2fcd0fc560b280acec16c6
akitotheanimator <sapressaonenem@gmail.com>
2026-03-26 11:06:14 -0300

    Create comparacao-ternaria.c

---
+++ comparacao-ternaria.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+float x = 29;
+float y = 30;
+int main()
+{
+    printf(x > y ? "Y é menor que X." : "X é menor que Y.");
+
+    return 0;
+}
